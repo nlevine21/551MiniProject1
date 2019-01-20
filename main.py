ee6d33dcd9e74c476cf5eafec99e7a33bb3e8c51
Vassili Korotkin <vkorotkin@gmail.com>
2019-01-20 18:27:20 -0500

    Add linear regression class
    Add class to solve linear regression. Gradient descent blows up on this dataset, unsure why so far.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from proj1_data_loading import loadData
 from construct_dataset import buildMatricies
 import numpy as np
 
+from matplotlib import pyplot as plt
 # Variables selecting which bonus features to add
 include_length = True
 include_num_sentences = True
@@ -19,4 +20,81 @@ X_training_data, Y_training_data = buildMatricies(training_data, include_length,
 X_validation_data, Y_validation_data = buildMatricies(validation_data, include_length, include_num_sentences)
 X_test_data, Y_test_data = buildMatricies(test_data, include_length, include_num_sentences)
 
-print (X_test_data[998])
+#print (X_test_data[998])
+
+
+
+class LinearRegression:
+    
+    """LinearRegression object. 
+    Attributes:
+        X: Input data. M by N numpy array. M is number of data points, N number features
+        Y: Output data. M by P numpy array. M number data points, P number of variables to predict. 
+        XtX: X transpose times X. Expensive to compute. Computed once on initialization, then reused.  
+    Notes: Y has to have two dimensions for the matrix multiplications to work. 
+                So, e.g. not (10,) but rather need (10,1)
+    """
+    def __init__(self, X, Y):
+        self.XtX=np.matmul(np.transpose(X),X)
+        self.X=X;
+        self.Y=Y;
+    def grad_e(self, w):
+        """Gradient of weights with respect to error. Returns N by 1 column matrix."""
+        return 2*(np.matmul(self.XtX, w)-np.matmul(np.transpose(self.X),self.Y))
+    def exact_solution(self):
+        """Computes the optimal weights for linear regression.
+            Returns weights vector w, N by P, where N number features, P number variables to predict
+            X: M by N matrix where M is number data points
+            Y: M by P matrix"""
+        w=np.matmul(np.linalg.inv(self.XtX),np.transpose(self.X))
+        w=np.matmul(w, self.Y)
+        return w
+    def gradient_descent(self, step_size, tol, max_iter):
+        """ Gradient descent, takes step_size (a function! that returns step size vs iteration)
+        and the maximum number of iterations."""
+        conv_flag=0
+        w=np.random.rand(self.X.shape[1],self.Y.shape[1])
+        grad_norms=np.ones([max_iter+1,1])
+        for iter in range(max_iter):
+            temp=w;
+            w=w-step_size(iter+1)*self.grad_e(w)
+            grad_norms[iter+1]=np.linalg.norm(self.grad_e(w))
+            if np.linalg.norm(w-temp)<tol:
+                conv_flag=1
+                break
+            
+        return w, grad_norms, conv_flag
+        
+
+X=X_training_data
+Y=np.asarray([Y_training_data]).T
+w=np.random.rand(X.shape[1],Y.shape[1])
+myLinReg=LinearRegression(X, Y)
+w=myLinReg.exact_solution()
+
+step_size=lambda k: 1/k**2
+max_iter=10
+tol=0.01
+
+w, grad_norms, conv_flag=myLinReg.gradient_descent(step_size, 0.01, max_iter)
+print(conv_flag)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
